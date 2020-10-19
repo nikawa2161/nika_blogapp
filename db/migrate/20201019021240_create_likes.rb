555c919fc38f301f2304208f282f337ba42ea759
nikawadori <nikawa.2161.t@icloud.com>
2020-10-19 12:22:36 +0900

    ?

--- db/migrate/20201019021240_create_likes.rb
+++ db/migrate/20201019021240_create_likes.rb
@@ -3,7 +3,6 @@ class CreateLikes < ActiveRecord::Migration[6.0]
     create_table :likes do |t|
       t.references :user, null: false
       t.references :article, null: false
-
       t.timestamps
     end
   end
