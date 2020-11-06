933a01447cdbde01a0c2d33a5f45a97589feac36
nikawadori <nikawa.2161.t@icloud.com>
2020-11-06 10:42:06 +0900

    decorator add

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,10 +45,6 @@ class User < ApplicationRecord
     likes.exists?(article_id: article.id)
   end
 
-  def display_name
-    self.email.split('@').first
-  end
-
   def follow!(user)
     user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
@@ -68,15 +64,7 @@ class User < ApplicationRecord
   def prepare_profile
     profile || build_profile
   end
-
-  def avatar_image
-    if profile&.avatar&.attached?
-      profile.avatar
-    else
-      'default-avatar.png'
-    end
-  end
-
+  
   private
   def get_user_id(user)
     if user.is_a?(User)
