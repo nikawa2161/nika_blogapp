7c574e1a684a688da075c11191918d1fb805a116
nikawadori <nikawa.2161.t@icloud.com>
2020-11-10 14:50:26 +0900

    コメント更新機能　追加

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,10 +1,7 @@
-
 import $ from 'jquery'
 import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
-
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
-
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
     $('.active-heart').removeClass('hidden')
@@ -12,32 +9,46 @@ const handleHeartDisplay = (hasLiked) => {
     $('.inactive-heart').removeClass('hidden')
   }
 }
-
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
-
   axios.get(`/articles/${articleId}/comments`)
-    .then((response) =>{
+    .then((response) => {
       const comments = response.data
-      comments.forEach(comment => {
-        $(`.comments-container`).append(
+      comments.forEach((comment) => {
+        $('.comments-container').append(
           `<div class="article_comment"><p>${comment.content}</p></div>`
         )
-      });
+      })
     })
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
 
-    $('.show-comment-form').on('click', () => {
-      $('.show-comment-form').addClass('hidden')
-      $('.comment-text-area').removeClass('hidden')
-    })
-  
+  $('.add-comment-button').on('click', () => {
+    const content = $('#comment_content').val()
+    if (!content) {
+      window.alert('コメントを入力してください')
+    } else {
+      axios.post(`/articles/${articleId}/comments`, {
+        comment: {content: content}
+      })
+        .then((res) => {
+          const comment = res.data
+          $('.comments-container').append(
+            `<div class="article_comment"><p>${comment.content}</p></div>`
+          )
+          $('#comment_content').val('')
+        })
+    }
+  })
+
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
     })
-    
   $('.inactive-heart').on('click', () => {
     axios.post(`/articles/${articleId}/like`)
       .then((response) => {
@@ -51,7 +62,6 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(e)
       })
   })
-
   $('.active-heart').on('click', () => {
     axios.delete(`/articles/${articleId}/like`)
       .then((response) => {
