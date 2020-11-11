1f70b94da1afa5759553f15d4518526ec28e22be
nikawadori <nikawa.2161.t@icloud.com>
2020-11-11 09:07:25 +0900

    mailer add

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -2,7 +2,7 @@ class RelationshipMailer < ApplicationMailer
   def new_follower(user, follower)
     @user = user
     @follower = follower
-    mail_to: 'user.email', subject: '【お知らせ】フォローされました'
+    mail to: user.email, subject: '【お知らせ】フォローされました'
   end
 
 end
